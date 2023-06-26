81ebc2171667fb2b24688fa0beb82a1343dce18b
solo <muroodaliev@gmail.com>
2023-06-26 21:34:45 +0500

    solo retry

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let n = +prompt('biror bir son kiriting');
 let power = +prompt('kiritgan sonni nechinchi darajaga oshirmoqchisiz?');
+let num = 1;
 while(isNaN(n) || n == 0) {
     alert("Birinchi kiritilgan ma'lumot xato");
     n = +prompt("qaytadan son kiriting");
 }
-while(isNaN(power)){
+while(isNaN(power) || power == 0){
     alert("darajaga oshirish uchun berilgan ma'lumot xato");
     power = +prompt('qaytadan son kiriting');
 }
-if(!isNaN(n) && !isNaN(power)){
-    alert("("+ n +")" + "^" + power + " = " + n**power);
+for(let i = 0; i < power; i++){
+    alert(num = num*n);
 }
